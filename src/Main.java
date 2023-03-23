c7cfb5af481c339840062621f7039cedba7b7ec3
gotokaka <kakaporider510@gmail.com>
2023-03-23 20:00:23 +0900

    Mapの追加

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 import java.util.List;
-/*生き物リストを作成し(List)、その中から鳥類のみの情報を取得する(Map)*/
+import java.util.HashMap;
+import java.util.Map;
+
+/*目標：生き物リストを作成し(List)、その中から鳥類のみの情報を取得する(Map)*/
 public class Main {
     public static void main(String[] args) {
         /*生き物インスタンスを生成する*/
@@ -14,6 +17,15 @@ public class Main {
         System.out.println(creatures.get(0));
         System.out.println(creatures.get(1));
         System.out.println(creatures.get(2));
+        /*Mapを追加。*/
+        HashMap<Integer, String> creaturesMap = new HashMap<>();
+        creaturesMap.put(0,"哺乳類");
+        creaturesMap.put(1,"鳥類");
+        creaturesMap.put(2,"魚類");
+
+        System.out.println(creaturesMap.get(0));
+        System.out.println(creaturesMap.get(1));
+        System.out.println(creaturesMap.get(2));
 
     }
 }
