5072a193fd9c21dc1fcdafc593419056ca7cc655
gotokaka <kakaporider510@gmail.com>
2023-03-24 22:21:16 +0900

    38行目のMapの修正
    keyを取得するメソッドにif文を追加した

--- src/Main.java
+++ src/Main.java
@@ -3,45 +3,52 @@ import java.util.List;
 import java.util.HashMap;
 import java.util.Map;
 
-/**目標：生き物リストを作成し(List)、その中から鳥類のみの情報を取得する(Map)
- * これから実行する事
- * 1.Mapでキーと値の両方を取得するメソッド(entrySet)を実行する
- * 2.Mapにおいて、「指定したキーが存在するか判定する」メソッド(containsKey)を実装してみる
- * 3.MapとListを使って目標の要件を満たしたコードを実装する
- * 4.例外処理を実装してみる**/
 public class Main {
     public static void main(String[] args) {
-        /*リストの宣言*/
+        /*Listの宣言*/
         List<String> creatures = new ArrayList<>();
         /*メソッドを定義する*/
-        creatures.add("キリン");
-        creatures.add("キジ");
-        creatures.add("マグロ");
-        /*出力する*/
-        System.out.println(creatures.size());
-        System.out.println(creatures.get(0));
-        System.out.println(creatures.get(1));
-        System.out.println(creatures.get(2));
-
-        System.out.println("------------------------------");
-
+        creatures.add("ゾウ");
+        creatures.add("タカ");
+        creatures.add("サンマ");
         /*Mapの宣言。*/
         Map< String,String> creaturesMap = new HashMap<>();
+        /*メソッドを定義する*/
         creaturesMap.put("哺乳類", "キリン");
         creaturesMap.put("鳥類", "キジ");
         creaturesMap.put("魚類", "マグロ");
-        /*Mapのkeyを取得するメソッド*/
-        for (String key : creaturesMap.keySet()) {
-            System.out.println(key);
+        creaturesMap.put("両生類", "カエル");
+
+        System.out.println("-----------------------------");
+
+        /*Listでの拡張for文*/
+        for(String value : creatures){
+            System.out.println(value);
         }
-        /*Mapの値(values)を取得するメソッド*/
-        for (String val : creaturesMap.values()) {
-            System.out.println(val);
-        }
+
+        System.out.println("＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝");
+
         /*Mapのkeyと値の両方を取るメソッド*/
         for (Map.Entry<String, String> entry : creaturesMap.entrySet()) {
             System.out.println(entry.getKey() + " : " + entry.getValue());
         }
 
+        System.out.println("＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝");
+
+        /*Mapのkeyを取得するメソッドとif文の追加*/
+        for (String key : creaturesMap.keySet()) {
+            if(key.equals("鳥類")){
+                System.out.println("キジは鳥類");
+            } else {
+                System.out.println(key +"は鳥類ではない");
+            }
+        }
+
+        System.out.println("＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝");
+
+        /*Mapの値(values)を取得するメソッド*/
+        for (String val : creaturesMap.values()) {
+            System.out.println(val);
+        }
     }
 }
