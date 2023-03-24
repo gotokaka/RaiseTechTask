5716c3844a2065264461408d9d8e535958b517f2
gotokaka <kakaporider510@gmail.com>
2023-03-24 17:41:17 +0900

    Mapのメソッドの変更
    Mapのkeyを取得するメソッドと値を取得するメソッドの追加。

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,12 @@ import java.util.List;
 import java.util.HashMap;
 import java.util.Map;
 
-/*目標：生き物リストを作成し(List)、その中から鳥類のみの情報を取得する(Map)*/
+/**目標：生き物リストを作成し(List)、その中から鳥類のみの情報を取得する(Map)
+ * これから実行する事
+ * 1.Mapでキーと値の両方を取得するメソッド(entrySet)を実行する
+ * 2.Mapにおいて、「指定したキーが存在するか判定する」メソッド(containsKey)を実装してみる
+ * 3.MapとListを使って目標の要件を満たしたコードを実装する
+ * 4.例外処理を実装してみる**/
 public class Main {
     public static void main(String[] args) {
         /*リストの宣言*/
@@ -25,10 +30,11 @@ public class Main {
         creaturesMap.put("哺乳類", "キリン");
         creaturesMap.put("鳥類", "キジ");
         creaturesMap.put("魚類", "マグロ");
-
+        /*Mapのkeyを取得するメソッド*/
         for (String key : creaturesMap.keySet()) {
             System.out.println(key);
         }
+        /*Mapの値(values)を取得するメソッド*/
         for (String val : creaturesMap.values()) {
             System.out.println(val);
         }
