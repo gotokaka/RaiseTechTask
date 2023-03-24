7556183b487a3480adca44e3742ea0248e1cac1d
gotokaka <kakaporider510@gmail.com>
2023-03-24 14:09:31 +0900

    Mapに拡張for文を追加

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.util.Map;
 /*目標：生き物リストを作成し(List)、その中から鳥類のみの情報を取得する(Map)*/
 public class Main {
     public static void main(String[] args) {
-        /*生き物インスタンスを生成する*/
+        /*リストの宣言*/
         List<String> creatures = new ArrayList<>();
         /*メソッドを定義する*/
         creatures.add("キリン");
@@ -17,15 +17,21 @@ public class Main {
         System.out.println(creatures.get(0));
         System.out.println(creatures.get(1));
         System.out.println(creatures.get(2));
-        /*Mapを追加。*/
-        HashMap<Integer, String> creaturesMap = new HashMap<>();
-        creaturesMap.put(0,"哺乳類");
-        creaturesMap.put(1,"鳥類");
-        creaturesMap.put(2,"魚類");
 
-        System.out.println(creaturesMap.get(0));
-        System.out.println(creaturesMap.get(1));
-        System.out.println(creaturesMap.get(2));
+        System.out.println("------------------------------");
+
+        /*Mapの宣言。*/
+        Map< String,String> creaturesMap = new HashMap<>();
+        creaturesMap.put("哺乳類", "キリン");
+        creaturesMap.put("鳥類", "キジ");
+        creaturesMap.put("魚類", "マグロ");
+
+        for (String key : creaturesMap.keySet()) {
+            System.out.println(key);
+        }
+        for (String val : creaturesMap.values()) {
+            System.out.println(val);
+        }
 
     }
 }
